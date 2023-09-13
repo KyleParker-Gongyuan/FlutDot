8ac6f2dea33af37ebcc3fa7a2bb395e976fd99a5
KyleParker-Gongyuan <67570672+KyleParker-Gongyuan@users.noreply.github.com>
2023-09-13 04:44:20 -0700

    re-implament websocket server
    OG websockets arent working (reason unkown). This works.
    A massive reimplantation of the OG websocket feature (reason 4 big commit)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/services.dart';
 import 'package:flutdot/flutdot.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
-late FlutDot FlutDotHandler;
+import 'dart:io'
+    show
+        HttpRequest,
+        HttpServer,
+        InternetAddress,
+        WebSocket,
+        WebSocketTransformer;
+import 'dart:convert' show json, utf8;
 
+late FlutDot FlutDotHandler;
+final List<WebSocket> onlineClients = [];
 void main() {
   FlutDotHandler = FlutDot();
   runApp(const MyApp());
@@ -16,9 +25,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
     return MaterialApp(
       title: 'flutdot',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: const HomePage(),
     );
   }
@@ -33,30 +48,34 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String godotData = "";
+  Color davecolor = Color.fromARGB(255,255,255,255);
   late MagnetometerEvent magEvent;
   @override
   void initState() {
     // TODO: implement initState
-    FlutDotHandler.cb = (data) {
-      godotData = data.toString();
-      setState(() {});
+    FlutDotHandler.receiveMsg = (data) {
+      //parseGameData(data);
+      setState(() {
+
+      });
+      godotData = data;
+      print(godotData);
     };
-    magnetometerEvents.listen((MagnetometerEvent event) { //! idk exactly why we have this
+    
+    /* magnetometerEvents.listen((MagnetometerEvent event) {
       magEvent = event;
-      print("MagnetometerEvent:" + event.toString());
-    });
+      //print("MagnetometerEvent:" + event.toString());
+    }); */
+
+    //initClient();
   }
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.landscapeLeft,
-      DeviceOrientation.landscapeRight,
-    ]);
     return (Scaffold(
       body: Stack(
         children: [
-          FlutDotHandler.GoDotContainerWidget(),
+          FlutDotHandler.GoDotContainer(),
           Align(
             alignment: Alignment(0.0, 0.9),
             child: Text(godotData),
@@ -64,14 +83,82 @@ class _HomePageState extends State<HomePage> {
           Align(
             alignment: Alignment(0.8, -0.8),
             child: TextButton(
-              child: Text("Send Hello Hero! -> GoDot"),
+              child: Text("Send Hello Hero! -> GoDot", style: TextStyle(color: davecolor),),
               onPressed: () {
-                FlutDotHandler.sendMessage("AlertData", "Hello Hero!");
+                //FlutDotHandler.sendMessage("AlertData", "Hello Hero!");
+                final a = {"command":"ping"};
+                
+                FlutDotHandler.sendMessage(json.encode(a));
+                
               },
             ),
           )
         ],
       ),
     ));
+  }
+}
+// a generic client 
+/* 
+void initClient() {
+  print("WS starting connection");
+  WebSocket.connect('ws://localhost:5000').then((WebSocket ws) {
+    // our websocket server runs on ws://localhost:8000
+    // in emulator might have to use 10.0.2.2:8000 instead of localhost
+    
+    if (ws.readyState == WebSocket.open) {
+      // as soon as websocket is connected and ready for use, we can start talking to other end
+      print("WS is open");
+      
+      ws.add(json.encode({
+        'data': 'from client at ${DateTime.now().toString()}',
+      })); // this is the JSON data format to be transmitted
+      ws.listen(
+        // gives a StreamSubscription
+        (data) { //! ideally we want to expose this json data to flutter it self 
+          
+          print("data from godot: " + 
+              '\t\t -- ${Map<String, String>.from(json.decode(data))}'); // listen for incoming data and show when it arrives
+          Timer(Duration(seconds: 1), () {
+            if (ws.readyState ==
+                WebSocket
+                    .open){ // checking whether connection is open or not, is required before writing anything on socket
+              ws.add(json.encode({
+                'data': 'from client at ${DateTime.now().toString()}',
+              }));
+            }
+          });
+        },
+        onDone: () => print('[+]Done :)'),
+        onError: (err) => print('[!]Error -- ${err.toString()}'),
+        cancelOnError: true,
+      );
+    } else
+      print('[!]Connection Denied');
+    // in case, if serer is not running now
+  }, onError: (err) => print('[!]Error -- ${err.toString()}'));
+  
+
+} */
+
+
+void parseGameData(String gameData){
+
+  final Map<String, dynamic> jsonMap = json.decode(gameData);
+
+  final String code = jsonMap['code'];
+  switch (code) {
+  case'ping':
+    print("game pinged server");
+    FlutDotHandler.sendMessage('{"command":"message", "val":"recieved msg"}');
+    break;
+  case'message':
+    print("message sent");
+    break;
+  default:
+    print("unknown code [${code}], CONTACT GAME DEV PPL!!!");
+    print("ALL CODE FLUSH:  [$gameData]");
+  
+  
   }
 }
--- lib/module/flutdot.dart
+++ lib/module/flutdot.dart
@@ -1,36 +1,54 @@
+import 'dart:async';
 import 'dart:convert';
+
 import 'dart:io';
+
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
+
 class FlutDot {
-  late int _localPort;
+  late int _localWebPort;
+  late int _localWSPort;
   late InAppLocalhostServer _localhostServer;
   late InAppWebViewController _webcontroller;
-  late Function(dynamic jsonData) cb;
 
-  ///Must be initialized in the void main(){}
-  ///sets the port for routing info to and from godot
-  FlutDot({int port = 1878}) {
-    _localPort = port;
+
+
+  /// [consoleCb] recives print(in godot) output's.
+  /// 
+  /// Needs more benchmarking to compare with real WS
+  late Function(dynamic jsonData) consoleCb;
+  
+  /// [receiveMsg] should*** be faster then console
+  late Function(String msg) receiveMsg;
+
+  final List<WebSocket> onlineClients = [];
+
+
+  ///Must be initialized in the void main(){} //! does it actually or do we need to be
+  FlutDot({int portWeb = 1878, int portWS = 5000}) {
+    _localWebPort = portWeb;
+    _localWSPort = portWS;
     if (!kIsWeb) {
-      _localhostServer = InAppLocalhostServer(port: _localPort);
+      _localhostServer = InAppLocalhostServer(port: _localWebPort);
     }
     // start the localhost server
     _startServer();
   }
 
   int get port {
-    return _localPort;
+    return _localWebPort;
   }
 
   void set port(int portNumber) {
-    _localPort = portNumber;
+    _localWebPort = portNumber;
   }
 
   void _startServer() async {
     // start the localhost server
+    
     try {
       WidgetsFlutterBinding.ensureInitialized();
       if (!kIsWeb) await _localhostServer.start();
@@ -41,34 +59,94 @@ class FlutDot {
     } catch (e) {
       print("FlotDotError: " + e.toString());
     }
+    //Start websocket Server
+    // maybe wait untill we get some code from server first
+    try {
+      
+      print("FlutDot: Init Server!");
+      
+      HttpServer.bind('localhost', _localWSPort).then((HttpServer server) { //! 不懂
+        print('FlutDot: [+]WebSocket listening at -- ws://localhost:$_localWSPort/');
+        server.listen((HttpRequest request) {
+          print("got request: $request");
+          WebSocketTransformer.upgrade(request).then((WebSocket ws) {
+            onlineClients.add(ws);
+            print("added client: $ws");
+            ws.listen(
+              (data) {
+                var curtim = DateTime.now();
+                print("recived somthing");
+
+                final utf8Decoder = utf8.decoder;
+                
+
+                final str = utf8Decoder.convert(data);
+                print(
+                    '\t\t FlutDot:${request?.connectionInfo?.remoteAddress} -- ${json.decode(str)}');
+
+                if (receiveMsg != null) {
+                  try {
+                    //dynamic jsonOBJ = jsonDecode(consoleMessage.message);
+                    receiveMsg(json.decode(str));
+                  } catch (e) {
+                    print("flutdot[!]consoleCallBack error: $e");
+                  }
+                }
+                Timer(Duration(seconds: 1), () {
+                  if (ws.readyState == WebSocket.open) {
+                    // checking connection state helps to avoid unprecedented errors
+                    ws.add(json.encode({
+                      'data': 'from server at ${DateTime.now().toString()}',
+                    }));
+                  }
+                }
+                
+                );
+              },
+              onDone: () => print('[+]Done :)'),
+              onError: (err) => print('[!]Error -- ${err.toString()}'),
+              cancelOnError: true,
+            );
+            ws.done.then((_) {
+                // Remove the disconnected client from the list.
+                onlineClients.remove(ws);
+              });
+          }, onError: (err) => print('[!]Error -- ${err.toString()}'));
+        }, onError: (err) => print('[!]Error -- ${err.toString()}'));
+      }, onError: (err) => print('[!]Error -- ${err.toString()}'));
+      print("started server");
+    } catch (e) {
+      print("FlotDotServerError: " + e.toString());
+    }
   }
 
-  Widget GoDotContainerWidget() {
+  Widget GoDotContainer() {
     return InAppWebView(
-      onConsoleMessage: (controller, consoleMessage) {
-        if (cb != null) {
-          try {
-            dynamic jsonOBJ = jsonDecode(consoleMessage.message);
-            cb(jsonOBJ);
-          } catch (e) {}
-        }
-      },
       onWebViewCreated: (controller) {
         _webcontroller = controller;
+      },
+      onConsoleMessage: (controller, consoleMessage) {
+        if (consoleCb != null) {
+          try {
+            //dynamic jsonOBJ = jsonDecode(consoleMessage.message);
+            consoleCb(consoleMessage.message);
+          } catch (e) {
+            print("flutdot[!]consoleCallBack error: $e");
+          }
+        }
       },
       initialUrlRequest:
           //URLRequest(url: Uri.parse("http://scooter-report.de/2d")),
           URLRequest(
               url: Uri.parse(
-                  "http://127.0.0.1:${_localPort}/GoDotExport/index.html")),
+                  "http://127.0.0.1:${_localWebPort}/GoDotExport/index.html")),
     );
   }
-
-  void sendMessage(String channel, String msg) {
-    if (_webcontroller != null) {
-      Map<String, String> rawdata = {"channel": "${channel}", "msg": "${msg}"};
-      _webcontroller.evaluateJavascript(
-          source: 'goDotJSChannel("${Uri.encodeFull(jsonEncode(rawdata))}");');
+  // send a message to all connected clients. (which is just 1 on locally)
+  void sendMessage(dynamic message) {
+    for (var client in onlineClients) {
+      client.addUtf8Text(utf8.encode(message));
     }
   }
+  
 }
